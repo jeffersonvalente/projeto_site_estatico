941001b4a78310776c5fb5b0cadd20a02dbfd44e
jeffersonvalente <jeffersonvalente1998@gmail.com>
2022-11-26 15:49:34 -0300

    adicionadada configuracao de cdn

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,9 @@
 #tags commons
 #caso não tenha um dominio, seta um dominio random_pet
 locals {
-  has_domain = var.domain != ""
-  domain     = local.has_domain ? var.domain : random_pet.website.id
+  has_domain      = var.domain != ""
+  domain          = local.has_domain ? var.domain : random_pet.website.id
+  regional_domain = module.website.regional_domain_name
 
   common_tags = {
     Project   = "Projeto Site Estatico"
