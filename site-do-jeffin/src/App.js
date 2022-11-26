e527cc35b664920dc494fd207dc7c678b4f8ac98
jeffersonvalente <jeffersonvalente1998@gmail.com>
2022-11-26 11:53:50 -0300

    adicionado a aplicação

---
+++ site-do-jeffin/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Projeto AWS.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/jefferson-hoy-valente/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Meu Linkedin
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
